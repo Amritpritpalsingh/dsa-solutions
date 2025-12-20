7e526b5838c54c335cdbe66d1c43ecb8571b03d4
Amritpritpalsingh <amritkps01@gmail.com>
2025-12-20 10:33:14 +0530

    Time: 3989 ms (8.33%), Space: 227.2 MB (10.32%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countPrimes(self, n):
+        l = [1 for i in range(n+1)]
+        for i in range(2,int(n**.5)+1):
+            if(l[i]!=0):
+                for j in range(i*i,len(l),i):
+                    l[j]=0
+        cnt = 0
+        for i in range(2,n):
+            if(l[i]==1):
+                cnt+=1
+        return cnt
+
+        
