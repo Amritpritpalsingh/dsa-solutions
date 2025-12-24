655c6a6fd1c498886cc2b4e8ea2d1636f161e036
Amritpritpalsingh <amritkps01@gmail.com>
2025-12-24 10:03:15 +0530

    Time: 47 ms (34.77%), Space: 14.3 MB (50.26%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def nextGreaterElements(self, nums):
+        n = len(nums)
+        nge = [-1] * n
+        st = []
+
+        for i in range(2*n - 1, -1, -1):
+            while st and st[-1] <= nums[i % n]:
+                st.pop()
+
+            if i < n:
+                nge[i] = st[-1] if st else -1
+
+            st.append(nums[i % n])
+
+        return nge
