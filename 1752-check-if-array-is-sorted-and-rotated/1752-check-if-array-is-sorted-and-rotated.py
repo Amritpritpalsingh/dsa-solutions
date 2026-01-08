d2f1510c3bcfed153276b35e62a23be1b4a3c836
Amritpritpalsingh <amritkps01@gmail.com>
2026-01-08 08:35:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,20 +1,33 @@
 class Solution(object):
     def check(self, nums):
-        dummy = nums[:]
-        dummy.sort()
+        # Brute Force 
+
+        # dummy = nums[:]
+        # dummy.sort()
+        # length = len(nums)
+        # for i in range(length):
+        #     flag = True
+        #     first = dummy[0]
+        #     for j in range(length - 1):
+        #         dummy[j] = dummy[j+1]
+        #     dummy[-1] = first
+        #     for k in range(length):
+        #         if(nums[k]!=dummy[k]):
+        #             flag = False
+        #             break
+        #     if(flag is True):
+        #         return True
+        # return False
+        
+        cnt = 1
         length = len(nums)
-        for i in range(length):
-            flag = True
-            first = dummy[0]
-            for j in range(length - 1):
-                dummy[j] = dummy[j+1]
-            dummy[-1] = first
-            for k in range(length):
-                if(nums[k]!=dummy[k]):
-                    flag = False
-                    break
-            if(flag is True):
+        for i in range((length*2)-1):
+            if(nums[i%length]<=nums[(i+1)%length]):
+                cnt+=1
+            else:
+                cnt = 1
+            if(cnt==length):
                 return True
         return False
-        
+
         
