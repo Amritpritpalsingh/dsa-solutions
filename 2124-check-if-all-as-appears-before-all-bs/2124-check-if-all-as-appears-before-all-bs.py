eeb456be77b021c634bbae0a69c6ce5131925ba5
Amritpritpalsingh <amritkps01@gmail.com>
2026-01-08 08:50:41 +0530

    Time: 0 ms (100%), Space: 12.4 MB (56.52%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def checkString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start_b = -1
+        n = len(s)
+        for i in range(n):
+            if(s[i]=="b"):
+                start_b = i
+                break
+    
+        flag = True
+        
+        if(start_b == -1):
+            return flag
+
+        for i in range(i,n):
+            if(s[i]=="a"):
+                flag = False
+                break
+        
+        return flag
+        
