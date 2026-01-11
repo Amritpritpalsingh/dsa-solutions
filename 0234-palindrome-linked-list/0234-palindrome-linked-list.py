feeafb5ae668e7c7fc165414f81cd61d4ab8670f
Amritpritpalsingh <amritkps01@gmail.com>
2026-01-11 16:23:46 +0530

    Time: 151 ms (16.41%), Space: 83.6 MB (41.79%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,20 +4,18 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
+   
     def isPalindrome(self, head):
-        """
-        :type head: Optional[ListNode]
-        :rtype: bool
-        """
-        dummy = head
+        dummay1 = head
         stack = []
-        while dummy:
-            stack.append(dummy)
-            dummy = dummy.next
-        for i in range(len(stack)):
-            if(stack[len(stack)-1-i].val == head.val):
-                head=head.next
-            else :
+        while(dummay1 is not None):
+            stack.append(dummay1.val)
+            dummay1 = dummay1.next
+
+        dummay2 = head
+        while(dummay2 is not None):
+            if stack[-1]!=dummay2.val:
                 return False
+            stack.pop()
+            dummay2 = dummay2.next
         return True
-        
