5c3da2481fefca2cba4bc949a1d02bc3c78793ed
Amritpritpalsingh <amritkps01@gmail.com>
2026-01-12 15:39:48 +0530

    Time: 6 ms (46.41%), Space: 13.1 MB (78.04%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,19 +1,26 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        l =[]
-        d = {}
-        i=0
-        for idx, num in enumerate(nums):
-            if target - num in d:
-                l.append(d[target - num])
-                l.append(idx)
-                break
-            d[num] = idx
-        return l
-    
         
+        map = nums[:]
+        nums.sort()
+        l_ptr = 0
+        r_ptr = len(nums)-1
+        while(l_ptr<r_ptr):
+            summation  = nums[l_ptr]+nums[r_ptr]
+            if(summation==target):
+                break
+            elif(summation>target):
+                r_ptr-=1
+            else:
+                l_ptr+=1
+        idx_first = -1
+        idx_sec = -1
+        for i in range(len(map)):
+            if(idx_first==-1 and nums[l_ptr]==map[i]):
+                idx_first = i
+            elif(idx_sec==-1 and nums[r_ptr]==map[i]):
+                idx_sec = i
+            if(idx_first!= -1 and idx_sec!= -1):
+                break
+        return [idx_first,idx_sec]
+                
