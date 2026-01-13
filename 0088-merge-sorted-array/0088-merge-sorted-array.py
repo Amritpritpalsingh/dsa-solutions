13df58d0bf76fe0c04d7e06d86296e6e4cd615f0
Amritpritpalsingh <amritkps01@gmail.com>
2026-01-13 20:18:20 +0530

    Time: 0 ms (100%), Space: 12.6 MB (17.74%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,29 +1,19 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
-        l_ptr = 0
-        r_ptr = 0
-        temp = [-1] * (m+n)
-        curr = 0
-        while l_ptr < m and r_ptr < n :
-            if nums1[l_ptr] <= nums2[r_ptr]:
-                temp[curr] = nums1[l_ptr]
-                l_ptr+=1
+        k_ptr = len(nums1)-1
+        l_ptr = m-1
+        r_ptr = n-1
+        while(r_ptr >=0):
+            if(l_ptr>=0 and nums1[l_ptr]>=nums2[r_ptr]):
+               
+                nums1[k_ptr] = nums1[l_ptr]
+                l_ptr-=1
             else:
-                temp[curr] = nums2[r_ptr]
-                r_ptr+=1
-            curr+=1
-        while l_ptr < m :
-            temp[curr] = nums1[l_ptr]
-            l_ptr+=1
-            curr+=1
+                nums1[k_ptr] = nums2[r_ptr]
+                r_ptr-=1
+                
 
-        while r_ptr < n :
-            temp[curr] = nums2[r_ptr]
-            r_ptr+=1
-            curr+=1
-        len_array = len(temp)
-        for i in range(len_array):
-            nums1[i] = temp[i]
+            k_ptr-=1
         
 
 
